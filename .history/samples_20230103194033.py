64c69dbdaa6b6f4fc04d7b99828f3cde456e1c36
Harun Kılıç <harunlokmank@gmail.com>
2023-05-19 20:26:16 +0300

    history... again.

--- .history/samples_20230103194033.py
+++
@@ -1,8 +0,0 @@
-import igraph as ig
-import matplotlib.pyplot as plt
-
-
-def sample1():
-    edges = [[0, 1], [1, 2], [0, 2], [0, 3], [3, 1]]
-    g = ig.Graph(n = 5, edges = edges)
-    return g
--- .history/samples_20230103194356.py
+++
@@ -1,9 +0,0 @@
-import igraph as ig
-import matplotlib.pyplot as plt
-
-
-def sample1():
-    edges = [[0, 1], [1, 2], [0, 2], [0, 3], [3, 1]]
-    g = ig.Graph(n = 5, edges = edges)
-    g.vs['name'] = str(g.vs)
-    return g
